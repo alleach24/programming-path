f29d8f05333262d32b1201a26a36e96e13f35de6
Andrea Leach <65141234+alleach24@users.noreply.github.com>
2022-12-20 23:45:30 -0500

    Delete programming_path/users directory

--- programming_path/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('signup/', views.sign_up),
-    # path('login/', views.log_in),
-    # path('logout/', views.log_out),
-    # path('whoami/', views.who_am_i),
-]
